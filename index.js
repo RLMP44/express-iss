af77f60ef551cf79d18c47c6e740169f6d298841
Rachael Momber <rlmomber@gmail.com>
2025-05-01 13:27:05 -0400

    refactor: update view to handle data from json file, change buttons

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import path from 'path';
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import countryCodes from "./country_codes.json" assert { type: "json" };
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -10,20 +12,28 @@ const app = express();
 const port = 3000;
 var satelliteData = {};
 var locData = {};
+var countryName = "";
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+function handleLocData(code) {
+  const country = countryCodes.codes.find((c) => c.Code === code);
+  return (country && !country === "??") ? country.Name : "an unknown location";
+}
 
 app.get('/', async (req, res) => {
   try {
     const response = await axios.get("https://api.wheretheiss.at/v1/satellites/25544");
     satelliteData = response.data;
     const locResponse = await axios.get(`https://api.wheretheiss.at/v1/coordinates/${satelliteData.latitude},${satelliteData.longitude}`);
+    countryName = (handleLocData(locResponse.data.country_code));
     locData = locResponse.data;
   } catch (error) {
     console.error("Error fetching data:", error.message);
-    res.status(500).json({ error: "Failed to fetch API data" });
+    return res.status(500).json({ error: "Failed to fetch API data" });
   }
-  res.render("index.ejs", { data: satelliteData, loc: locData });
+  res.render("index.ejs", { data: satelliteData, loc: locData, name: countryName });
 })
 
 app.listen(port, () => {
