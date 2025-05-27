063b8c330d9ad117fede1e6b555e6184ac9eeec6
Rachael Momber <rlmomber@gmail.com>
2025-05-27 13:36:42 -0400

    feat: make stats toggleable

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,15 @@
 function scrollNextCard(cards, currentIndex) {
   return currentIndex === cards.length - 1 ? 0 : currentIndex + 1;
-}
+};
 
 function scrollPreviousCard(cards, currentIndex) {
   return currentIndex === 0 ? cards.length - 1 : currentIndex - 1;
-}
+};
 
 // need to use .each because jQuery returns an object
 $('.arrow-button').each(function() {
   $(this).on('click', function(event) {
-    const currentButton = $(this)
+    const currentButton = $(this);
     const arrow = event.target.closest('.arrow-button');
     const cardIndex = parseInt(arrow.getAttribute('data-card-index'));
     const cards = JSON.parse(arrow.getAttribute('data-cards'));
@@ -17,5 +17,15 @@ $('.arrow-button').each(function() {
     const newIndex = currentButton.hasClass('right-button') ? scrollNextCard(cards, cardIndex) : scrollPreviousCard(cards, cardIndex) ;
     $(`#card-${cardIndex}`).addClass("hidden");
     $(`#card-${newIndex}`).removeClass("hidden");
+  });
+});
+
+$('.toggle-button').each(function() {
+  $(this).on('click', function(event) {
+    $('.live').toggleClass('mx-3');
+    $('.main-content').toggleClass('d-none');
+    $('.toggle').toggleClass('extra-left-margin');
+    $('.toggle').toggleClass('left');
+    $('.toggle').toggleClass('right');
   });
 });
